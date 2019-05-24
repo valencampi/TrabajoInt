30942abd4baa8d8362c58ad7ae98d39e5a190b09
Manuel <manuabrulchab@gmail.com>
2019-05-24 14:54:52 -0300

    JS Y HTML DE GENEROS CON FETCH

---
+++ generos.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+
+  fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=6e11caa7480bb2cb18a5bff7908d4f53&language=en-US")
+  .then(function(respond) {
+    return respond.json()
+  })
+  .then(function(data) {
+
+    var arrayGeneros = data.genres
+    console.log(data.genres);
+
+    var generos=document.querySelector("ul.generos")
+
+    for (var i = 0; i < 5; i++) {
+      var name = arrayGeneros[i].name
+
+      generos.innerHTML += "<li>"+ name + "</li>"
+
+
+    }
+  })
+  .catch(function(error) {
+    console.log("Error" + error) ;
+  })
+
+  }
