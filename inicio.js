a265b5767d6f9358438fde53bb0784711cfc935e
Valentina <vcampitelli@udesa.edu.ar>
2019-06-03 16:59:29 -0300

    Carrousel funciona brooo

--- inicio.js
+++ inicio.js
@@ -33,12 +33,12 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=6e11caa7480bb2cb18a5bf
   return respond.json()
 })
 .then(function(data) {
-  console.log(data);
+  // console.log(data);
 
   var cantidadDePelis = 6;
 
   var arrayPopular = data.results
-  console.log(arrayPopular);
+  // console.log(arrayPopular);
 
   var populares=document.querySelector("ul.populares")
 
@@ -82,13 +82,13 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=6e11caa7480bb2cb18a5
   return respond.json()
 })
 .then(function(data) {
-  console.log(data);
-  console.log("puntaje");
+  // console.log(data);
+  // console.log("puntaje");
 
   var cantidadDePelis = 6;
 
   var arrayPuntaje = data.results
-  console.log(arrayPuntaje);
+  // console.log(arrayPuntaje);
 
   var puntaje=document.querySelector("ul.puntaje")
   var li = ""
@@ -96,9 +96,9 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=6e11caa7480bb2cb18a5
   var div = ""
 
   for (var i = 0; i < 6; i++) {
-    var id = arrayPuntaje[i].id
-    var title = arrayPuntaje[i].title
-    var foto = arrayPuntaje[i].poster_path
+    var id = arrayPuntaje[i].id;
+    var title = arrayPuntaje[i].title;
+    var foto = arrayPuntaje[i].poster_path;
 
     if (i === 0) {
       li = "<li class='mySlides1' style='display:block;height:100%;'>"
@@ -113,7 +113,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=6e11caa7480bb2cb18a5
     puntaje.innerHTML += li
 
     div = "<div class='column1'>"
-      div += "<img class='demo cursor' style='width:100%' onclick='currentSlide(1)' src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
+      div += "<img class='demo1 cursor' style='width:100%' onclick='currentSlide1(1)' src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
     div += "</div>"
 
     thumbnail.innerHTML += div
@@ -131,13 +131,13 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=6e11caa7480bb2cb18a5b
   return respond.json()
 })
 .then(function(data) {
-  console.log(data);
-  console.log("estrenos");
+  // console.log(data);
+  // console.log("estrenos");
 
   var cantidadDePelis = 6;
 
   var arrayEstrenos = data.results
-  console.log(arrayEstrenos);
+  // console.log(arrayEstrenos);
 
   var estrenos = document.querySelector("ul.estrenos")
   var li = ""
@@ -163,7 +163,7 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=6e11caa7480bb2cb18a5b
   estrenos.innerHTML += li
 
   div = "<div class='column2'>"
-    div += "<img class='demo cursor' style='width:100%' onclick='currentSlide(1)' src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
+    div += "<img class="+'demo2 cursor'+" style="+'width:100%'+" onclick="+'currentSlide2(1)'+" src="+'https://image.tmdb.org/t/p/w500/'+ foto + ">"
   div += "</div>"
 
   thumbnail.innerHTML += div
@@ -181,3 +181,32 @@ function myFunction() {
 
 
 }
+
+/*Busqueda*/
+
+ fetch("https://api.themoviedb.org/3/search/movie?api_key=6e11caa7480bb2cb18a5bff7908d4f53&language=en-US&page=1&include_adult=false")
+ .then(function(respond) {
+   return respond.json()
+ })
+
+ // then(function(data) {
+ //
+ //   console.log(data);
+ //   console.log("busqueda");
+ //
+ //   var arrayDeBusqueda = data.results
+ //   console.log(arrayDeBusqueda);
+ //
+ //   for (var i = 0; i < 5; i++){
+ //   var title = arrayDeBusqueda[i].title
+ //   var foto = arrayDeBusqueda[i].poster_path
+ //
+ //   estrenos.innerHTML += "<ul>" + title + "</ul>"
+ //   estrenos.innerHTML += "<img src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
+ //
+ // }
+ // )
+ //
+ // .catch(function(error) {
+ //   return console.log("Error" + error);
+ // })
--- iniciocar.js
+++ iniciocar.js
@@ -1,5 +1,5 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -14,17 +14,14 @@ function currentSlide(n) {
 function showSlides(n) {
   console.log(n);
   var i;
+  // capturo el array de <li>
   var slides = document.getElementsByClassName("mySlides");
+  // capturo las <img> pequeñas que estás debajo.
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1
-    console.log("primer if");
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-    console.log("segundo if");
-  }
+  //si
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -42,7 +39,7 @@ function showSlides(n) {
 // carrousel 2
 
 var slideIndex1 = 1;
-showSlides1(slideIndex1);
+// showSlides1(slideIndex1);
 
 // Next/previous controls
 function plusSlides1(n) {
@@ -59,52 +56,56 @@ function showSlides1(n) {
   var slides = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("demo1");
   var captionText = document.getElementById("caption1");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active1", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  console.log(slideIndex);
-  console.log(slides[slideIndex-1]);
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active1";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  console.log(slideIndex1);
+  console.log(slides[slideIndex1-1]);
+  slides[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
+  captionText.innerHTML = dots[slideIndex1-1].alt;
 }
 
 // carrousel 3
 
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
+var slideIndex3 = 1;
+// showSlides2(slideIndex3);
 
 // Next/previous controls
-function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
+function plusSlides2(x) {
+  showSlides2(slideIndex3 += x);
 }
 
 // Thumbnail image controls
-function currentSlide2(n) {
-  showSlides2(slideIndex2 = n);
+function currentSlide2(x) {
+  showSlides2(slideIndex3 = x);
 }
 
-function showSlides2(n) {
+function showSlides2(x) {
+  console.log(slideIndex3);
+  console.log(x);
   var i;
+  //capturo los <li>
   var slides = document.getElementsByClassName("mySlides2");
+  //capturo
   var dots = document.getElementsByClassName("demo2");
   var captionText = document.getElementById("caption2");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (x > slides.length) {slideIndex3 = 1}
+  if (x < 1) {slideIndex3 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active2", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  console.log(slideIndex2);
-  console.log(slides[slideIndex-1]);
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active2";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  console.log(slideIndex3);
+  console.log(slides[slideIndex3-1]);
+  slides[slideIndex3-1].style.display = "block";
+  dots[slideIndex3-1].className += " active";
+  captionText.innerHTML = dots[slideIndex3-1].alt;
 }
