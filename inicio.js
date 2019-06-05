69e27d7d97411bacbb8a2818069ecd742fb6f025
Valentina <vcampitelli@udesa.edu.ar>
2019-06-05 14:55:02 -0300

    Auto stash before revert of "Pagina Login"
    conflicto

--- inicio.js
+++ inicio.js
@@ -122,79 +122,14 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=6e11caa7480bb2cb18a5b
   // console.log(data);
   // console.log("estrenos");
 
-  var cantidadDePelis = 6;
-
-  var arrayEstrenos = data.results
-  // console.log(arrayEstrenos);
-
-  var estrenos = document.querySelector("ul.estrenos")
-  var li = ""
-  var thumbnail =document.querySelector("div.row2")
-  var div = ""
-
-  for (var i = 0; i < 6; i++){
-  var id = arrayEstrenos[i].id
-  var title = arrayEstrenos[i].title
-  var foto = arrayEstrenos[i].poster_path
-
-  if (i === 0) {
-    li = "<li class='mySlides2' style='display:block;height:100%;'>"
-  }else {
-    li = "<li class='mySlides2'>"
-  }
-
-    li += "<div class='numbertext2'>" +(i+1) + "/" +cantidadDePelis+ " <div/>"
-    li += "<p class='opacidad'>"+ title + "</p>"
-    li += "<img src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
-  li += "</li>"
-
-  estrenos.innerHTML += li
-
-  div = "<div class='column2'>"
-    div += "<img class="+'demo2 cursor'+" style="+'width:100%'+" onclick="+'currentSlide2(1)'+" src="+'https://image.tmdb.org/t/p/w500/'+ foto + ">"
-  div += "</div>"
-
-  thumbnail.innerHTML += div
-}
-})
-.catch(function(error) {
-  return console.log("Error" + error);
-})
-
-
-/*LogIn*/
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-
-
-}
-
-/*Busqueda*/
-
- fetch("https://api.themoviedb.org/3/search/movie?api_key=6e11caa7480bb2cb18a5bff7908d4f53&language=en-US&page=1&include_adult=false")
- .then(function(respond) {
-   return respond.json()
- })
-
- // then(function(data) {
- //
- //   console.log(data);
- //   console.log("busqueda");
- //
- //   var arrayDeBusqueda = data.results
- //   console.log(arrayDeBusqueda);
- //
- //   for (var i = 0; i < 5; i++){
- //   var title = arrayDeBusqueda[i].title
- //   var foto = arrayDeBusqueda[i].poster_path
- //
- //   estrenos.innerHTML += "<ul>" + title + "</ul>"
- //   estrenos.innerHTML += "<img src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
- //
- // }
- // )
- //
- // .catch(function(error) {
- //   return console.log("Error" + error);
- // })
+  /*LogIn*/
+//   function myFunction() {
+//     // document.getElementById("demo").innerHTML = "Hello World";
+//     // ACA TIENE QUE REDIRIGIR AL USUARIO A LA PAGINAD DE INGRESO
+//     // BUSCAR COMO REDIRECCIONAR AL USUARIO window.location.href
+//   }
+//
+//     li += "<div class='numbertext2'>" +(i+1) + "/" +cantidadDePelis+ " <div/>"
+//     li += "<p class='opacidad'>"+ title + "</p>"
+//     li += "<img src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
+//   li += "</li>"
