b49a62d8e95e3b6faa35e09b8034cb3b2aeb15e7
Manuel <manuabrulchab@gmail.com>
2019-06-21 00:03:01 -0300

    dsp de tanto, el push final

--- PeliculasPreferidas.js
+++ PeliculasPreferidas.js
@@ -1,5 +1,31 @@
 window.addEventListener("load",function () {
 
+  //media query event handler
+  if(matchMedia){
+      const mq = window.matchMedia( "(max-width:768px)" );//sets the width you want
+      mq.addListener(WidthChange);
+      WidthChange(mq)
+  }
+
+  function WidthChange(mq){
+      if(mq.matches){
+          //if window width is less than or equal to 768px;
+          $("#navbar_toggle").removeClass("dis-none"); //makes toggler visible
+          $("#navbar_collapse").addClass("dis-none");//makes the menu not visible initially
+          //so you can only see the menu items when you click the toggler
+
+          $("#navbar_toggle").on('click', function(e){
+              $("#navbar_collapse").slideToggle();
+              $("#navbar_collapse").css({"display": "block"});
+          })
+      }else{
+          //for larger screens
+          $("#navbar_toggle").addClass('dis-none');
+          $("#navbar_collapse").removeClass('dis-none');
+      }
+  }
+
+
 
 var arrayPeliculas= JSON.parse(window.sessionStorage.getItem("favorita"))
 console.log(arrayPeliculas);
@@ -45,6 +71,7 @@ console.log(arrayPeliculas[i]);
 
 
 }
+})
 
 
 
@@ -90,34 +117,3 @@ function agregarFavoritos(id){
   console.log(JSON.parse(window.sessionStorage.getItem("favorita")));
 
 }
-
-
-
-  //media query event handler
-  if(matchMedia){
-      const mq = window.matchMedia( "(max-width:768px)" );//sets the width you want
-      mq.addListener(WidthChange);
-      WidthChange(mq)
-  }
-
-  function WidthChange(mq){
-      if(mq.matches){
-          //if window width is less than or equal to 768px;
-          $("#navbar_toggle").removeClass("dis-none"); //makes toggler visible
-          $("#navbar_collapse").addClass("dis-none");//makes the menu not visible initially
-          //so you can only see the menu items when you click the toggler
-
-          $("#navbar_toggle").on('click', function(e){
-              $("#navbar_collapse").slideToggle();
-              $("#navbar_collapse").css({"display": "block"});
-          })
-      }else{
-          //for larger screens
-          $("#navbar_toggle").addClass('dis-none');
-          $("#navbar_collapse").removeClass('dis-none');
-      }
-  }
-
-
-
-})
